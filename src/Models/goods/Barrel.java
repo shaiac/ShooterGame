9f59b84edbe268b816c73fb70f68fe314cc45581
shaiac <shai.acoca@gmail.com>
2020-07-27 20:49:51 +0300

    Jack Collision

--- src/Models/goods/Barrel.java
+++ src/Models/goods/Barrel.java
@@ -1,26 +1,35 @@
 package Models.goods;
 
+import CollisionDetection.CollisionData;
+import CollisionDetection.ICollisionObj;
 import Models.DataAndLoader.ObjData;
 import Models.DataAndLoader.ObjectLoader;
 import Models.Model;
 
 import javax.media.opengl.GL2;
 
-public class Barrel extends Model {
+public class Barrel extends Model implements ICollisionObj {
     private String path;
+    private CollisionData collisionData;
+
     public Barrel(String path) {
         this.path = path;
     }
 
     @Override
     public void create(ObjectLoader loader, GL2 gl, float[] startPos) {
-        data = loader.LoadModelToGL(path,gl);
+        data = loader.LoadModelToGL(path,gl, "AABB");
+        this.collisionData = loader.getCollisionData();
         this.translate(1f,0f,0f);
         this.startPos = startPos;
+        this.collisionData.setStartPos(startPos);
+        float[] scale = {0.05f,0.05f,0.05f};
+        this.collisionData.setScale(scale);
     }
 
     @Override
     public void draw(GL2 gl) {
+        collisionData.draw(gl);
         gl.glPushMatrix();
         gl.glTranslatef(startPos[0],startPos[1],startPos[2]);
         gl.glScalef(0.05f,0.05f,0.05f);
@@ -28,6 +37,15 @@ public class Barrel extends Model {
             obj.draw(gl);
         }
         gl.glPopMatrix();
+    }
 
+    @Override
+    public void collide() {
+
+    }
+
+    @Override
+    public CollisionData getCollisionData() {
+        return collisionData;
     }
 }
